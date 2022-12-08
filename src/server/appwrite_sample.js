d307450dc418a9fe39c6284b3a3f8f38f0813c20
Edward <director@goodwebple.co.kr>
2022-12-08 20:09:12 +0000

    Add Appwrite Sample

---
+++ src/server/appwrite_sample.js
@@ -0,0 +1,7 @@
+import { Client } from "appwrite";
+
+const client = new Client()
+    .setEndpoint('') // Your API Endpoint
+    .setProject('');               // Your project ID
+
+export default client
